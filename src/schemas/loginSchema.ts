b81bd6b37eb28a97794a47a2b91e84b9c8573454
Lo <lostreit@pm.me>
2026-04-07 14:11:27 +0200

    featurmade schema properties email and userbame optional and added .refine to handle empty input values

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -1,6 +1,9 @@
 import z from "zod"
 
 z.object({
-  email: z.string(),
+  email: z.string().optional(),
+  username: z.string().optional(),
   password: z.string()
+}).refine(data => data.email || data.username, {
+  message: "Email or username is required to login"
 })
